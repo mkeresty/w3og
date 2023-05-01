1a4600a656f2f09c856e757aa7ff221f5f4b75f5
derp <derp@derpfinance.com>
2023-05-01 09:12:03 -0400

    deleted unnessary stuff

--- launchGanache.sh
+++
@@ -1,1 +0,0 @@
-ganache -e 10000  -m "nice fetch insect exact south wheel stay pepper section piece tenant select"
--- migrations/2_deploy.js
+++
@@ -1,6 +0,0 @@
-const derpContract = artifacts.require("MYERC721");
-
-module.exports = async function(deployer){
-    await deployer.deploy(derpContract);
-    console.log("MYERC721 Address: " + derpContract.address);
-};
